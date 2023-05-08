0d19213f8ad4cb2995a17de29faf522f097ca662
hebrown <hbrown@alumni.scu.edu>
2023-05-07 22:47:57 -0700

    Additional comments

--- src/addTemplate.ts
+++ src/addTemplate.ts
@@ -2,14 +2,16 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import { exec } from 'child_process';
 
+//Uses GH CLI extension to add a new file to the repository
+//After file is added, view switches to the newly created file
 export async function addFile(resource: vscode.Uri, template: string, context: vscode.ExtensionContext) {
-  const templateFile = template + '.md'
+  const templateFile = template + '.md';
 
   const destinationFolderUri = resource.with({ scheme: 'file' });
   const destinationFolderPath = destinationFolderUri.fsPath;
   const filePath = vscode.Uri.file(destinationFolderPath).fsPath;
 
-  const command = 'gh doctemplate ' + templateFile + ' -o ' + filePath
+  const command = 'gh doctemplate ' + templateFile + ' -o ' + filePath;
   await new Promise<void>((resolve, reject) => {
     exec(command, (error, stdout, stderr) => {
       if (error) {
@@ -21,7 +23,7 @@ export async function addFile(resource: vscode.Uri, template: string, context: v
     });
   });
 
-  const createdFile = path.join(filePath, templateFile)
-  const document = await vscode.workspace.openTextDocument(createdFile)
+  const createdFile = path.join(filePath, templateFile);
+  const document = await vscode.workspace.openTextDocument(createdFile);
   vscode.window.showTextDocument(document);
 }
