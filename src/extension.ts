3f4fec262619116251008b19d47337848b7905ed
Henry Brown <hbrown@alumni.scu.edu>
2023-09-08 18:17:23 -0700

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,7 @@ import { insertTemplate } from './insertTemplate';
 import { listTemplates } from './listTemplates';
 
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// Your extension is activated the very first time the command is executed.
 export function activate(context: vscode.ExtensionContext) {
 
 	//Command palette template select
